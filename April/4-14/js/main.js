4b570ef6b45c686c89e299cd48a34bd805fee18f
Sergey <skot1990@hotmail.com>
2022-05-01 17:47:36 -0400

    cw 5-1-22

---
+++ April/4-14/js/main.js
@@ -0,0 +1,5 @@
+function opposite(number) {
+    //your code here
+    number = number * -1
+    return number
+  }
---
+++ April/4-20/js/main.js
@@ -0,0 +1,12 @@
+function nameShuffler(str){
+    // Shuffle It
+    let arr = str.split(' ')
+    
+    return arr[1] + ' ' + arr[0]
+  }
+
+  
+// codewars solution
+// function nameSuffle(str){
+//     return str.split(' ').reverse().join(' ')
+//   }
